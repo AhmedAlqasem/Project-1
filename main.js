9a09f97eb8b325c6b1b0a1239b233846d084fa8a
AhmedAlqasem <ahmedalqasem93@gmail.com>
2021-04-25 06:10:39 +0300

    HTML+CSS

---
+++ main.js
@@ -0,0 +1,37 @@
+/*====================SPORT QUASTIONS=================
+
+1)Queen's berry rules ' is the name given to the rules in
+RIGHT ANSWER: Boxing
+Cricket
+ Tennis
+Hockey	
+
+2)  Which of the following International Tennis Tournaments is held on grass court?
+RIGHT ANSWER C) Wimbledon
+
+A) US open	B) French Open
+	D) Australian Open
+
+*/
+
+
+const examType = function (type){
+    if(type==="Math")
+    window.location.href="./Math/Math.html";
+    if(type==="Sport")
+    window.location.href="./Sports/Sports.html";
+    if(type==="History")
+    window.location.href="./History/History.html";
+
+}
+
+
+
+// RIGHT ANSWER: Boxing
+// Cricket
+//  Tennis
+// Hockey	
+
+
+
+
